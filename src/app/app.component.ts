e499f8a959eb56bebe4b20c4fa2ac1c097feef2c
davidzabalperez <davidzabalperez@gmail.com>
2019-03-06 19:30:30 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { Employee } from './models/employee';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+
+
+  employeeArray: Employee[] =[
+
+    {id: 1,
+    name: "David",
+    country: "Donosti"
+    },
+    {id: 2,
+    name: "Ander",
+    country: "Irun"
+    },
+    {id: 3,
+    name: "Arantxa",
+    country: "Renteria"
+    }
+  ];
+
+  seletedEmployee: Employee = new Employee();
+
+  addOrEdit(){
+    this.seletedEmployee.id = this.employeeArray.length + 1;
+    this.employeeArray.push(this.seletedEmployee);
+    this.seletedEmployee = new Employee();
+  }
 }
